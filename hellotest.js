a0021bc6d63f46ce00d087db8e803a6f42565628
smithg7 <smithg7@oregonstate.edu>
2016-05-15 17:15:11 -0600

    test

--- hellotest.js
+++ hellotest.js
@@ -15,6 +15,16 @@ app.get('/other-page',function(req,res){
   res.render('other-page');
 });
 
+function genContext(){
+  var stuffToDisplay = {};
+  stuffToDisplay.time = (new Date(Date.now())).toLocaleTimeString('en-US');
+  return stuffToDisplay;
+}
+
+app.get('/time',function(req,res){
+  res.render('time', genContext());
+});
+
 app.use(function(req,res){
   res.status(404);
   res.render('404');
@@ -27,15 +37,7 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-function genContext(){
-  var stuffToDisplay = {};
-  stuffToDisplay.time = (new Date(Date.now())).toLocaleTimeString('en-US');
-  return stuffToDisplay;
-}
 
-app.get('/time',function(req,res){
-  res.render('time', genContext());
-});
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
