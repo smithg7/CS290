98644001b2ee51fd5e00a638156961bc8521a624
smithg7 <smithg7@oregonstate.edu>
2016-05-15 17:08:03 -0600

    test

--- hellotest.js
+++ hellotest.js
@@ -1,30 +1,30 @@
 var express = require('express');
 
 var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.get('/',function(req,res){
-  res.type('text/plain');
-  res.send('Welcome to the main page!');
+  res.render('home.handlebars') //We can omit the .handlebars extension as we do below
 });
 
 app.get('/other-page',function(req,res){
-  res.type('text/plain');
-  res.send('Welcome to the other page!');
+  res.render('other-page');
 });
 
 app.use(function(req,res){
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.type('plain/text');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function(){
