11279fad2fde965a36fa5b657f84fcacacecedea
smithg7 <smithg7@oregonstate.edu>
2016-05-15 18:11:33 -0600

    Assignment 3

---
+++ Assignment 3/CS290-Assignment3.js
@@ -0,0 +1,69 @@
+/*
+Name: Gary Smith
+CS290 HW Assignment: GET and POST
+
+NOTE: This code has been copied from the example given in class
+*/
+
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.render('home.handlebars') //We can omit the .handlebars extension as we do below
+});
+
+app.get('/other-page',function(req,res){
+  res.render('other-page');
+});
+
+
+
+app.get('/loopback',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render('get-loopback', context);
+});
+
+app.post('/loopback', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post-loopback', context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
