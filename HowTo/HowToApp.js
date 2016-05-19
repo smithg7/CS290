cf76c17e45b92dbe3897f504525e0cc40c8b6229
smithg7 <smithg7@oregonstate.edu>
2016-05-18 22:00:27 -0600

    test

--- HowTo/HowToApp.js
+++ HowTo/HowToApp.js
@@ -1,24 +1,19 @@
 var express = require('express');
 
 var app = express();
+
+
 app.set('port', 3000);
 
-app.get('/howTo',function(req,res){
-  res.send('HowTo.html');
+app.get('/', function(req, res){
+	res.sendfile('HowTo.html');
 });
 
-app.use(function(req,res){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
-});
-
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.send('500 - Server Error');
+app.use(function(req, res, next){
+        res.type('text/plain');
+        res.status(404);
+        res.send('404 - There is nothing here save for vast expanses of nothing');
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+        console.log( 'Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to exit' );});
