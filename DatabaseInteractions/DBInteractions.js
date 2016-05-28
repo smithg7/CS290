54073fdef7a5e589d15b59feb4f72fa2bcf26bad
smithg7 <smithg7@oregonstate.edu>
2016-05-28 16:43:41 -0600

    db

--- DatabaseInteractions/DBInteractions.js
+++ DatabaseInteractions/DBInteractions.js
@@ -34,6 +34,7 @@ app.get('/',function(req,res){
 //This route handles a GET request
 //and returns the get-loopback template
 app.post('/', function(req,res){
+  alert("huh");
   var qParams = [];
   //get all the body parameters
   for (var p in req.body){
