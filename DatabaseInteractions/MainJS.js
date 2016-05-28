4cca450ca556c0ad394e6550d101a0fd69369a1d
smithg7 <smithg7@oregonstate.edu>
2016-05-28 16:55:10 -0600

    DB

---
+++ DatabaseInteractions/MainJS.js
@@ -0,0 +1,47 @@
+// CS290 Web Development
+// Author: Gary Smith
+// Activity - Ajax Interactions
+
+var appid = "fa7d80c48643dfadde2cced1b1be6ca1";
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons() {
+    //Add event listener for the first submit button.
+    document.getElementById('exerciseSubmit').addEventListener('click', function (event) {
+        var req = new XMLHttpRequest();
+        var url = '/';
+        var payload = { name: null, reps: null, weight: null, date: null, lbs: null};
+
+        payload.name = document.getElementById('name').value;
+        payload.reps = document.getElementById('reps').value;
+        payload.weight = document.getElementById('weight').value;
+        payload.date = document.getElementById('date').value;
+        payload.lbs = document.getElementById('lbs').value;
+        
+        //Request data via a post.
+        req.open('POST', url, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+
+        //Add Event lister for the response.
+        req.addEventListener('load', function () {
+            if (req.status >= 200 && req.status < 400) {
+                var response = JSON.parse(req.responseText);
+                console.log(JSON.parse(req.responseText));
+
+                //Populate the result into the string.
+                var returnedData = JSON.parse(response.data);
+                var resultString = "Your Name: " + returnedData.last + ', ' + returnedData.first + '\n';
+                resultString += "You are " + returnedData.age + ' years old. \n';
+                document.getElementById('echoresults').textContent = resultString;
+
+            } else {
+                console.log("Error in network request: " + request.statusText);
+            }
+        });
+
+        //Send the request with the data entered in the form.
+        req.send(JSON.stringify(payload));
+        event.preventDefault();
+    });
+
+};
