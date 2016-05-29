715533da671f29326f8bb171eb149ebdc96dd061
smithg7 <smithg7@oregonstate.edu>
2016-05-29 10:31:41 -0600

    db

--- DatabaseInteractions/public/MainJS.js
+++ DatabaseInteractions/public/MainJS.js
@@ -30,7 +30,7 @@ function bindButtons() {
                 //clear out the table and replace it with the database info
                 var DBTable = JSON.parse(req.responseText);
                 for (var row in DBTable) {
-                  console.log(JSON.stringify(DBTable[row]));
+                  console.log(JSON.stringify(DBTable[row].Ename));
                 }
                 
              } else {
