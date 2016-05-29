15d3af6184605a8c7d351edb42a4c3d8a1cb7bab
smithg7 <smithg7@oregonstate.edu>
2016-05-29 14:45:08 -0600

    db

--- DatabaseInteractions/public/MainJS.js
+++ DatabaseInteractions/public/MainJS.js
@@ -46,6 +46,11 @@ function EditBtn(Eid)
 { 
     var thisForm = document.getElementById('EditForm'+Eid);
     console.log("The value you typed is: " + thisForm.elements["newOne"].value);
+    console.log("The value you typed is: " + thisForm.elements["Hname"].value);
+    console.log("The value you typed is: " + thisForm.elements["Hreps"].value);
+    console.log("The value you typed is: " + thisForm.elements["Hweight"].value);
+    console.log("The value you typed is: " + thisForm.elements["Hdate"].value);
+    console.log("The value you typed is: " + thisForm.elements["Hlbs"].checked);
 
 
 
@@ -120,23 +125,23 @@ function PopulateTable(data)
     //Add all the new rows back in
     for (var row in data) {
         var newRow = pageTable.insertRow();
-        var rowHTML = "<td><input type='text' style='visibility:hidden;' id='Vname" + data[row].id + "' value='";
+        var rowHTML = "<td><input type='text' style='visibility:hidden;width:10em;' id='Vname" + data[row].id + "' value='";
         rowHTML += data[row].Ename + "' onchange='copyToHidden(" + data[row].id + ")' />";
         rowHTML += data[row].Ename + "</td>";
 
-        rowHTML += "<td><input type='text' class='hiding' id='Vreps" + data[row].id + "' value='";
+        rowHTML += "<td><input type='text' style='visibility:hidden;width:10em;' id='Vreps" + data[row].id + "' value='";
         rowHTML += data[row].reps + "' onchange='copyToHidden(" + data[row].id + ")' />";
         rowHTML += data[row].reps + "</td>";
 
-        rowHTML += "<td><input type='text' class='hiding' id='Vweight" + data[row].id + "' value='";
+        rowHTML += "<td><input type='text' style='visibility:hidden;width:10em;' id='Vweight" + data[row].id + "' value='";
         rowHTML += data[row].weight + "' onchange='copyToHidden(" + data[row].id + ")' />";
         rowHTML += data[row].weight + "</td>";
 
-        rowHTML += "<td><input type='text' class='hiding' id='Vdate" + data[row].id + "' value='";
+        rowHTML += "<td><input type='text' style='visibility:hidden;width:10em;' id='Vdate" + data[row].id + "' value='";
         rowHTML += data[row].date + "' onchange='copyToHidden(" + data[row].id + ")' />";
         rowHTML += data[row].date + "</td>";
 
-        rowHTML += "<td><input type='checkbox' class='hiding' id='Vlbs" + data[row].id + "' checked=";
+        rowHTML += "<td><input type='checkbox' style='visibility:hidden;' id='Vlbs" + data[row].id + "' checked=";
         rowHTML += data[row].lbs + " onchange='copyToHidden(" + data[row].id + ")' />";
         rowHTML += data[row].lbs + "</td>";
 
