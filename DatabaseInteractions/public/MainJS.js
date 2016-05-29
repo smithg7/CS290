048685ab04f5925cc4ddece1c184bc6b9742e3f8
smithg7 <smithg7@oregonstate.edu>
2016-05-29 14:58:30 -0600

    db

--- DatabaseInteractions/public/MainJS.js
+++ DatabaseInteractions/public/MainJS.js
@@ -125,7 +125,7 @@ function PopulateTable(data)
     //Add all the new rows back in
     for (var row in data) {
         var newRow = pageTable.insertRow();
-        var rowHTML = "<td><input type='text' style='visibility:hidden;' id='Vname" + data[row].id + "' value='";
+        var rowHTML = "<td><input type='text' style='visibility:hidden;size=30;' id='Vname" + data[row].id + "' value='";
         rowHTML += data[row].Ename + "' onchange='copyToHidden(" + data[row].id + ")' />";
         rowHTML += data[row].Ename + "</td>";
 
