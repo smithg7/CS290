09bbbb64c4121fb6bf465d5b6c3b6b1ef47f8d9c
smithg7 <smithg7@oregonstate.edu>
2016-05-29 10:48:15 -0600

    db

--- DatabaseInteractions/public/MainJS.js
+++ DatabaseInteractions/public/MainJS.js
@@ -33,12 +33,12 @@ function bindButtons() {
 
                 for (var row in DBTable) {
                     var newRow = pageTable.insertRow();
-                    var cell1 = row.insertCell(0);
-                    var cell2 = row.insertCell(1);
-                    var cell3 = row.insertCell(2);
-                    var cell4 = row.insertCell(3);
-                    var cell5 = row.insertCell(4);
-                    var cell6 = row.insertCell(5);
+                    var cell1 = newRow.insertCell(0);
+                    var cell2 = newRow.insertCell(1);
+                    var cell3 = newRow.insertCell(2);
+                    var cell4 = newRow.insertCell(3);
+                    var cell5 = newRow.insertCell(4);
+                    var cell6 = newRow.insertCell(5);
 
                     cell1.text = DBTable[row].Ename
                     cell2.text = DBTable[row].reps
