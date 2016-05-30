43194eda5b3ebea93fcaf3bab5a2c5c40b49db71
smithg7 <smithg7@oregonstate.edu>
2016-05-29 18:22:37 -0600

    db

--- DatabaseInteractions/public/MainJS.js
+++ DatabaseInteractions/public/MainJS.js
@@ -140,7 +140,7 @@ function PopulateTable(data)
 
         rowHTML += "<td><input type='checkbox' class='hiding' id='Vlbs" + data[row].id + "' checked=";
         rowHTML += data[row].lbs + " onchange='copyToHidden(" + data[row].id + ")' />";
-        rowHTML += "<div class='showing' id='lbsData" + data[row].id + "'>" + data[row].lbs + "</div></td>";
+        rowHTML += "<div class='showing' id='lbsData" + data[row].id + "'><input type='checkbox' checked=" + data[row].lbs + " disabled /></div></td>";
 
         rowHTML += "<td><form id='EditForm" + data[row].id + "'>";
         rowHTML += "<input type='hidden' id='Hname" + data[row].id + "' value='"+data[row].Ename+"' />";
